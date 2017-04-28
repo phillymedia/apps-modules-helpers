a0a5fd159423adeeb00e5f4146b8a54780586a58
bakerej <ebaker@philly.com>
2017-04-28 17:52:36 -0400

    again

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ const env = process.env.NODE_ENV
 // grab debug mode
 const debug = process.env.DEBUG
 	// (we use a separate debugmode process so as not to turn on the debug mode in OTHER modules)
-	|| process.env.DEBUGMODE
+	|| (process.env.DEBUGMODE === "true")
 	// npm
 	|| (process.env.npm_config_debugmode === "true");
 
@@ -26,7 +26,7 @@ const Main = {
 };
 console.log("debug", typeof Main.debug, Main.debug);
 // print information about environment.
-if (Main.debug === true) {
+if (Main.debug) {
 	console.log("Running helpers app in", Main.env, "mode. Debug is on.");
 }
 // export completed config file
