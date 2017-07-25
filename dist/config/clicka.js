334c16ca1ce565008493d84b421f6fb3f4b396fc
bakerej <ebaker@philly.com>
2017-07-25 17:48:16 -0400

    UPDATE: more clicka options.

---
+++ dist/config/clicka.js
@@ -0,0 +1,1 @@
+"use strict";var Main={};Main.clickability={},Main.clickability.errors={badCredentials:"PERMISSION_DENIED",duplicateUser:"DUPLICATE_VALUE",incomplete:"MISSING_VALUE"},module.exports=Main;
